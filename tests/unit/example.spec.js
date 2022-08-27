8ae5fe5a828af9aa3fe0a85d8761fe2c2b8efb18
miniflycn <miniflycn@justany.net>
2022-08-27 11:39:32 +0800

    fixed test bug

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -4,25 +4,18 @@ import { warn } from 'vue';
 
 jest.setTimeout(10000)
 describe('HelloWorld.vue', () => {
-  it('校验第一个按钮的spm是否为aa.bb.cc', (done) => {
-    const wrapper = shallowMount(HelloWorld);
+  it('校验第一个按钮的spm是否为aa.bb.cc', async () => {
+    const wrapper = shallowMount(HelloWorld, { attachTo: document.body });
     const button = wrapper.findAll('button');
-    button[0].trigger('click');
-    new Promise(rs => {
-      setTimeout(() => {
-        rs(null);
-      }, 1000);
-    }).then(() => {
-      expect(wrapper.vm.spmText).toMatch('1');
-      done();
-    })
+    await button[0].trigger('click');
+    expect(wrapper.vm.spmText).toMatch('aa.bb.cc');
   })
 })
-// describe('HelloWorld.vue', () => {
-//   it('校验第一个按钮的spm是否为aa.dd.ff', () => {
-//     const wrapper = shallowMount(HelloWorld)
-//     const button = wrapper.findAll('button')
-//     button[1].trigger('click')
-//     expect(wrapper.vm.spmText).toMatch('aa.dd.ff')
-//   })
-// })
+describe('HelloWorld.vue', () => {
+  it('校验第一个按钮的spm是否为aa.dd.ff', async () => {
+    const wrapper = shallowMount(HelloWorld, { attachTo: document.body })
+    const button = wrapper.findAll('button')
+    await button[1].trigger('click')
+    expect(wrapper.vm.spmText).toMatch('aa.dd.ff')
+  })
+})
