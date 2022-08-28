6b41c0aba9ec10f33b0c5b6eada527a4cfbca8c2
blankzz <454257312@qq.com>
2022-08-29 01:19:39 +0800

    feat(homework9): 完成 homework9

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="hello" data-spma="aa">
-    <span>show spm:{{spmText}}</span>
+  <div ref="wrapperRef" class="hello" data-spma="aa">
+    <span>show spm:{{ spmText }}</span>
     <div data-spmb="bb">
       <button data-spmc="cc">Click it</button>
     </div>
@@ -10,16 +10,10 @@
   </div>
 </template>
 
-<script>
+<script setup>
+import { useSpm } from "./hooks/useSpm";
 // TODO 利用事件代理实现一个简单的收集spm信息的方法，注意不是针对每一个按钮进行函数绑定。场景：考虑一下如果一个页面中有很多按钮，需要如何处理
-export default {
-  name: 'HelloWorld',
-  data: ()=>{
-    return {
-      spmText: 'xx.xx.xx'
-    }
-  }
-}
+const { spmText, wrapperRef } = useSpm();
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
