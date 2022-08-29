f005544d94aacfbced1346e394219ac4b6ef9460
xiaoquan <xiaoquan@qunhemail.com>
2022-08-30 00:05:45 +0800

    complete homework9

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,6 +11,8 @@
 </template>
 
 <script>
+
+
 // TODO 利用事件代理实现一个简单的收集spm信息的方法，注意不是针对每一个按钮进行函数绑定。场景：考虑一下如果一个页面中有很多按钮，需要如何处理
 export default {
   name: 'HelloWorld',
@@ -18,7 +20,42 @@ export default {
     return {
       spmText: 'xx.xx.xx'
     }
+  },
+  methods: {
+    findSpm(target, spmObj) {
+      const data = target.dataset
+      if (data && Object.keys(data)?.length) {
+        Object.keys(data).map(key => {
+          if (['spma', 'spmb', 'spmc'].includes(key) && !spmObj[key]) {
+            spmObj[key] = data[key]
+          }
+        })
+      }
+      if (target.parentNode) {
+        this.findSpm(target.parentNode, spmObj)
+      }
+      return spmObj['spma'] +`${spmObj['spmb'] && '.'}` + spmObj['spmb'] + `${spmObj['spmc'] && '.'}` + spmObj['spmc']
+    },
+    logClick(e) {
+      const spmObj = {
+        'spma': '',
+        'spmb': '',
+        'spmc': ''
+      }
+      const text = this.findSpm(e?.target, spmObj)
+      if (text) {
+        this.spmText = text
+      }
+    }
+  },
+  
+  created: function() {
+    document.body.addEventListener('click', this.logClick)
+  },
+  unmounted: function() {
+    document.body.removeEventListener('click', this.logClick)
   }
+  
 }
 </script>
 
